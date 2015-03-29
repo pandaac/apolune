5248c95e7ef131be3c662b1599a648834fec8676
Christopher Eklund <eklundchristopher@gmail.com>
2015-03-29 06:53:24 +0200

    Split

--- build/split.sh
+++ build/split.sh
@@ -1,4 +1,4 @@
 git subsplit init git@github.com:pandaac/apolune.git
-git subsplit publish --heads="master" src/Apolune/core:git@github.com:Apolune/core.git
-git subsplit publish --heads="master" src/Apolune/account:git@github.com:Apolune/account.git
+git subsplit publish --heads="master" src/Apolune/Core:git@github.com:apolune/core.git
+git subsplit publish --heads="master" src/Apolune/Account:git@github.com:apolune/account.git
 rm -rf .subsplit/
