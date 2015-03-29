6694ff6fb80bd1598a7afbd423cd70f969ef6deb
Christopher Eklund <eklundchristopher@gmail.com>
2015-03-29 01:12:17 +0100

    Initial commit

---
+++ src/Http/Controllers/AuthController.php
@@ -0,0 +1,128 @@
+<?php namespace pandaac\Account\Http\Controllers;
+
+use pandaac\Core\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\Registrar;
+
+class AuthController extends Controller {
+
+	/**
+	 * The Guard implementation.
+	 *
+	 * @var Guard
+	 */
+	protected $auth;
+
+	/**
+	 * The registrar implementation.
+	 *
+	 * @var Registrar
+	 */
+	protected $registrar;
+
+	/**
+	 * The prefixed route.
+	 *
+	 * @var string
+	 */
+	protected $prefix;
+
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
+	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+	 * @return void
+	 */
+	public function __construct(Guard $auth, Registrar $registrar)
+	{
+		$this->auth = $auth;
+		$this->registrar = $registrar;
+		$this->prefix = $this->getRouter()->getCurrentRoute()->getPrefix();
+
+		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+	/**
+	 * Show the application registration form.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getRegister()
+	{
+		return view('pandaac/account::auth.register');
+	}
+
+	/**
+	 * Handle a registration request for the application.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function postRegister(Request $request)
+	{
+		$validator = $this->registrar->validator($request->all());
+
+		if ($validator->fails())
+		{
+			$this->throwValidationException(
+				$request, $validator
+			);
+		}
+
+		$this->auth->login($this->registrar->create($request->all()));
+
+		return redirect($this->prefix);
+	}
+
+	/**
+	 * Show the application login form.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getLogin()
+	{
+		return view('pandaac/account::auth.login');
+	}
+
+	/**
+	 * Handle a login request to the application.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function postLogin(Request $request)
+	{
+		$this->validate($request, [
+			'account' => 'required', 'password' => 'required',
+		]);
+
+		$credentials = $request->only('account', 'password');
+
+		if ($this->auth->attempt($credentials, $request->has('remember')))
+		{
+			return redirect()->intended($this->prefix);
+		}
+
+		return redirect($this->prefix)
+					->withInput($request->only('remember'))
+					->withErrors([
+						'account' => 'These credentials do not match our records.',
+					]);
+	}
+
+	/**
+	 * Log the user out of the application.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getLogout()
+	{
+		$this->auth->logout();
+
+		return redirect($this->prefix);
+	}
+
+}
