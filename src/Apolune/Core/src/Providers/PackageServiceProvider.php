815af65258a16d255d3154a8b431ea3ee4dd16f8
Christopher Eklund <eklundchristopher@gmail.com>
2015-03-31 15:11:53 +0200

    Theme structural changes

---
+++ src/Apolune/Core/src/Providers/PackageServiceProvider.php
@@ -0,0 +1,11 @@
+<?php namespace Apolune\Core\Providers;
+
+use Apolune\Core\Contracts\Package;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+abstract class PackageServiceProvider extends BaseServiceProvider implements Package {
+
+	// ...
+
+}
