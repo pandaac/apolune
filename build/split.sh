db9b4f982be05490585caf5cc6f353a7025c9bfe
Kristoffer Eklund <kristoffereklund1@gmail.com>
2015-05-07 00:37:58 +0200

    Composer changes

--- build/split.sh
+++ build/split.sh
@@ -1,4 +1,5 @@
 git subsplit init git@github.com:pandaac/apolune.git
 git subsplit publish --heads="master" src/Apolune/Core:git@github.com:apolune/core.git
 git subsplit publish --heads="master" src/Apolune/Account:git@github.com:apolune/account.git
+git subsplit publish --heads="master" src/Apolune/Library:git@github.com:apolune/library.git
 rm -rf .subsplit/
