cdb1538e5a8b7c6ced806c66bae27b85d56a889f
Christopher Eklund <eklundchristopher@gmail.com>
2015-05-10 23:46:42 +0200

    Server component

--- build/split.sh
+++ build/split.sh
@@ -3,4 +3,5 @@ git subsplit publish --heads="master" src/Apolune/Account:git@github.com:apolune
 git subsplit publish --heads="master" src/Apolune/Contracts:git@github.com:apolune/contracts.git
 git subsplit publish --heads="master" src/Apolune/Core:git@github.com:apolune/core.git
 git subsplit publish --heads="master" src/Apolune/Library:git@github.com:apolune/library.git
+git subsplit publish --heads="master" src/Apolune/Server:git@github.com:apolune/server.git
 rm -rf .subsplit/
