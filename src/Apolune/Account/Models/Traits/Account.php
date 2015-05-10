401176cf59c86bd3f86c1414c2abc57a413b9573
Christopher Eklund <eklundchristopher@gmail.com>
2015-05-10 15:15:09 +0200

    Account trait

---
+++ src/Apolune/Account/Models/Traits/Account.php
@@ -0,0 +1,26 @@
+<?php namespace Apolune\Account\Models\Traits;
+
+use Apolune\Core\Traits\AuthenticatableTraits;
+use Apolune\Contracts\Account\Traits\Account as AccountContract;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model implements AccountContract {
+
+	use AuthenticatableTraits;
+
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = '__pandaac_accounts';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = ['remember_token'];
+
+}
