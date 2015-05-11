499d1f96fc58980e0edf4b978a99b514212cdfdb
Christopher Eklund <eklundchristopher@gmail.com>
2015-05-11 20:16:45 +0200

    Apolune Service Provider

---
+++ src/Apolune/ApoluneServiceProvider.php
@@ -0,0 +1,29 @@
+<?php namespace Apolune;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApoluneServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+	
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->register('Apolune\Account\Providers\AccountServiceProvider');
+		$this->app->register('Apolune\Server\Providers\ServerServiceProvider');
+		$this->app->register('Apolune\Support\Providers\SupportServiceProvider');
+	}
+	
+}
