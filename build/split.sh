558757a03b4dad2c64fb1a27a0b46f441ac3c016
Christopher Eklund <eklundchristopher@gmail.com>
2015-06-13 21:40:30 +0200

    News component

--- build/split.sh
+++ build/split.sh
@@ -3,6 +3,7 @@ git subsplit publish --heads="master" src/Apolune/Account:git@github.com:apolune
 git subsplit publish --heads="master" src/Apolune/Contracts:git@github.com:apolune/contracts.git
 git subsplit publish --heads="master" src/Apolune/Core:git@github.com:apolune/core.git
 git subsplit publish --heads="master" src/Apolune/Library:git@github.com:apolune/library.git
+git subsplit publish --heads="master" src/Apolune/News:git@github.com:apolune/news.git
 git subsplit publish --heads="master" src/Apolune/Server:git@github.com:apolune/server.git
 git subsplit publish --heads="master" src/Apolune/Support:git@github.com:apolune/support.git
 rm -rf .subsplit/
