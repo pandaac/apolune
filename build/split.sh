0e8e6ba87e8f29c82f8af03eaef6efd07138733d
Christopher Eklund <eklundchristopher@gmail.com>
2015-08-21 12:53:44 +0200

    Community packages build split

--- build/split.sh
+++ build/split.sh
@@ -3,8 +3,15 @@ git subsplit publish --heads="master" src/Apolune/About:git@github.com:apolune/a
 git subsplit publish --heads="master" src/Apolune/Account:git@github.com:apolune/account.git
 git subsplit publish --heads="master" src/Apolune/Contracts:git@github.com:apolune/contracts.git
 git subsplit publish --heads="master" src/Apolune/Core:git@github.com:apolune/core.git
+git subsplit publish --heads="master" src/Apolune/Guilds:git@github.com:apolune/guilds.git
+git subsplit publish --heads="master" src/Apolune/Highscore:git@github.com:apolune/highscore.git
+git subsplit publish --heads="master" src/Apolune/Houses:git@github.com:apolune/houses.git
 git subsplit publish --heads="master" src/Apolune/Library:git@github.com:apolune/library.git
 git subsplit publish --heads="master" src/Apolune/News:git@github.com:apolune/news.git
+git subsplit publish --heads="master" src/Apolune/Polls:git@github.com:apolune/polls.git
+git subsplit publish --heads="master" src/Apolune/Profile:git@github.com:apolune/profile.git
 git subsplit publish --heads="master" src/Apolune/Server:git@github.com:apolune/server.git
+git subsplit publish --heads="master" src/Apolune/Statistics:git@github.com:apolune/statistics.git
 git subsplit publish --heads="master" src/Apolune/Support:git@github.com:apolune/support.git
+git subsplit publish --heads="master" src/Apolune/Worlds:git@github.com:apolune/worlds.git
 rm -rf .subsplit/
